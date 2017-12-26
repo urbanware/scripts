71bceddd283e05fd5ff8f200e71bc149bc3f63b3
Walter Spicer <ror@urbanware.com>
2017-12-26 15:18:49 -0500

    Added docstring

--- days_from_now.py
+++ days_from_now.py
@@ -4,6 +4,9 @@ from datetime import datetime
 from datetime import timedelta
 
 def days_from_now():
+    """
+    Display the date after a certain number of days.
+    """
     temp = int(input("Enter number of days: "))
     date = (datetime.now() + timedelta(days=temp))
     print(date.strftime("%A %B %d %Y"))
